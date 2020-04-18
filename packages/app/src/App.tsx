bb502d2dd6d360d2488a81ec870f7f4dd6a412ef
kiyoshimori <darkf0x13566@gmail.com>
2020-04-18 17:04:27 +0300

    init monorepo project

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { test } from '@shop/ui-kit';
+
+function App() {
+  console.log(test);
+  return (
+    <div>
+      test
+    </div>
+  );
+}
+
+export default App;
