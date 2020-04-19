0e4beadc69208bd1e010c0906bbfc005f64c0a27
kiyoshimori <darkf0x13566@gmail.com>
2020-04-19 13:44:45 +0300

    fix sass modules and add prettier to eslint

--- packages/app/src/pages/Main/Main.tsx
+++ packages/app/src/pages/Main/Main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import { Test } from '@ui-kit/test';
+
+import styles from './Main.module.scss';
 
 export function Main() {
   return (
-    <div>
-      {Test()}
+    <div className={styles.wrapper}>
+      <Test />
     </div>
   );
 }
---
+++ packages/ui-kit/test/src/Test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Test.module.scss';
+
+export function Test() {
+  return (
+    <div className={styles.wrapper}>Test</div>
+  );
+}
